95720e3686e0db652e111d43f537924cccb1856b
tom_glb <goodMorning_glb@jinyu.com>
2021-04-17 20:15:43 +0800

    delete all

--- src/main/java/com/atjy/jwt/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.atjy.jwt.mapper;
-
-import com.atjy.jwt.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper {
-    User login(User user);
-}
--- src/main/java/com/atjy/jwt/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.atjy.jwt.service;
-
-import com.atjy.jwt.entity.User;
-
-public interface UserService {
-    public User login(User user);
-}
